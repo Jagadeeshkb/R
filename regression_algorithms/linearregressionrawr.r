77a1c82c97286eb05506d1d935524b7461dfafba
Jagadeesh <kbjagadeesh6297@gmail.com>
2024-07-08 12:37:11 +0530

    Changes made in Regression file

--- regression_algorithms/linearregressionrawr.r
+++ regression_algorithms/linearregressionrawr.r
@@ -1,6 +1,7 @@
 ols<-function(y,x){
-    data<-model.matrix(y ~ ., data = x)
+    data<-model.matrix(y ~ x, data = x)
     decomp <- svd(data)
+    
     return(decomp$v %*% diag(1 / decomp$d) %*% t(decomp$u) %*% y)
   }
 
@@ -8,3 +9,11 @@ set.seed(1)
 x <- rnorm(1000)
 y <- 4 * x + rnorm(1000, sd = .5)
 ols(y=y,x=matrix(x, ncol = 1))
+
+svd_vec = svd(matrix(x,ncol=1))
+str(svd_vec)
+library(lobstr)
+diag(svd_vec$d)
+
+svd_vec$v %*% diag(1 / svd_vec$d) %*% t(svd_vec$u) %*% y
+
